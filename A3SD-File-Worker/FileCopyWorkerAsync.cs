7ceaec3cbd4659e901ad221633d499f99dc4e123
Caleb Serafin <calebserafin@outlook.com>
2020-10-18 01:17:28 +0200

    Re-factored FileCopyWorkerAsync to be more usable.

--- A3SD-File-Worker/FileCopyWorkerAsync.cs
+++ A3SD-File-Worker/FileCopyWorkerAsync.cs
@@ -1,3 +1,8 @@
+// <copyright file="Program.cs" company="Caleb Sebastian Serafin">
+// Copyright (c) 2020 Caleb Sebastian Serafin. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the repository root for full license information.
+// </copyright>
+
 using System.Collections.Concurrent;
 using System.IO;
 using System.Threading;
@@ -5,40 +10,78 @@ using System.Threading.Tasks;
 
 namespace A3SD_File_Worker {
 	public class FileCopyWorkerAsync {
-		public FileCopyWorkerAsync() {
+		public bool ReplaceAll;
+		public int concurrentTasks;
+		public int readWriteStreamBufferSize;
+		private readonly ConcurrentQueue<InOutPath> copyJobs = new ConcurrentQueue<InOutPath>();
+
+		public FileCopyWorkerAsync(bool ReplaceAll = false, int concurrentTasks = 6, int readWriteStreamBufferSize = 32_768) {
+			this.ReplaceAll = ReplaceAll;
+			this.concurrentTasks = concurrentTasks;
+			this.readWriteStreamBufferSize = readWriteStreamBufferSize;
 		}
 
 		public void ApplyLargeCopyOptimisationPreset() {
 			concurrentTasks = 2;
-			readWriteStreamBuffer = 16_777_216;
+			readWriteStreamBufferSize = 16_777_216;
 		}
 
-		private ConcurrentQueue<InOutPath> copyJobs = new ConcurrentQueue<InOutPath>();
-		private int concurrentTasks = 6;
-		public int readWriteStreamBuffer = 32_768;
-		public bool ReplaceAll = false;
+		public async Task StartJobs(CancellationToken cancel) {
+			CancellationTokenSource streamComplete = new CancellationTokenSource();
+			Task[] ThreadedTasks = new Task[concurrentTasks];
+			try {
+				for (int i = 0; i < concurrentTasks; i++) {
+					ThreadedTasks[i] = CreateCopyTask(streamComplete.Token, cancel);
+				}
+				streamComplete.Cancel();
+				await Task.WhenAll(ThreadedTasks);
+			} finally {
+				streamComplete.Dispose();
+			}
+		}
 
-		private struct InOutPath {
-			public string source, output;
-			public InOutPath(string source, string output) {
-				this.source = source;
-				this.output = output;
+		public void EnqueueJob(string source, string output) => EnqueueJob(new InOutPath(source, output));
+		public void EnqueueJob(InOutPath job) {
+			if (File.Exists(job.output)) {
+				EnqueueFileJob(job);
+			} else if (Directory.Exists(job.source)) {
+				EnqueueDirectoryJob(job);
+			} else {
+				throw new DirectoryNotFoundException("Could not find Directory/File '" + job.source + "'");
+			}
+		}
+
+		public void EnqueueDirectoryJob(string source, string output) => EnqueueDirectoryJob(new InOutPath(source, output));
+		public void EnqueueDirectoryJob(InOutPath job) {
+			DirectoryInfo sourceDir = new DirectoryInfo(job.source);
+			foreach (DirectoryInfo dir in sourceDir.EnumerateDirectories("*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = true })) {
+				Directory.CreateDirectory(Path.Combine(job.output, Path.GetRelativePath(job.source, dir.FullName)));
+			}
+			foreach (FileInfo file in sourceDir.EnumerateFiles("*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = true })) {
+				EnqueueFileJob(new InOutPath(file.FullName, Path.Combine(job.output, Path.GetRelativePath(job.source, file.FullName))));
+			}
+		}
+
+		public void EnqueueFileJob(string source, string output, bool createDirectory = false) => EnqueueFileJob(new InOutPath(source, output), createDirectory);
+		public void EnqueueFileJob(InOutPath job, bool createDirectory = false) {
+			if (ReplaceAll || new FileInfo(job.source).LastWriteTime > new FileInfo(job.output).LastWriteTime) {
+				if (createDirectory) Directory.CreateDirectory(Path.GetDirectoryName(job.output));
+				copyJobs.Enqueue(job);
 			}
 		}
 
 		private async Task CreateCopyTask(CancellationToken streamComplete, CancellationToken cancel) {
-			FileStream reader;
-			FileStream writer;
-			InOutPath job;
 			bool hasDelayed = false;
 			while (!cancel.IsCancellationRequested) {
-				if (copyJobs.TryDequeue(out job)) {
-					hasDelayed = false;
-					reader = new FileStream(job.source, FileMode.Open, FileAccess.Read, FileShare.Read, readWriteStreamBuffer, true);
-					writer = new FileStream(job.output, FileMode.Create, FileAccess.Write, FileShare.None, readWriteStreamBuffer, true);
-					await reader.CopyToAsync(writer, cancel);
-					await reader.DisposeAsync();
-					await writer.DisposeAsync();
+				if (copyJobs.TryDequeue(out InOutPath job)) {
+					{
+						hasDelayed = false;
+						using FileStream reader = new FileStream(job.source, FileMode.Open, FileAccess.Read, FileShare.Read, readWriteStreamBufferSize, true);
+						using FileStream writer = new FileStream(job.output, FileMode.Create, FileAccess.Write, FileShare.None, readWriteStreamBufferSize, true);
+						await reader.CopyToAsync(writer, cancel);
+						await reader.DisposeAsync();
+						await writer.DisposeAsync();
+					}
 				} else {
 					if (hasDelayed && streamComplete.IsCancellationRequested) break;
 					hasDelayed = true;
@@ -46,35 +89,32 @@ namespace A3SD_File_Worker {
 				}
 			}
 		}
+	}
 
-		public async Task CreateCopyJobs(string sourcePath, string outputPath, CancellationToken cancel) {
-			DirectoryInfo sourceDir = new DirectoryInfo(sourcePath);
-			if (!sourceDir.Exists) {
-				throw new DirectoryNotFoundException("Could not find '" + sourcePath + "'");
-			}
-			foreach (DirectoryInfo dir in sourceDir.EnumerateDirectories("*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = true })) {
-				Directory.CreateDirectory(Path.Combine(outputPath, Path.GetRelativePath(sourcePath, dir.FullName)));
-			}
+	public struct InOutPath {
+		public string source, output;
+		public InOutPath(string source, string output) {
+			this.source = source;
+			this.output = output;
+		}
 
-			CancellationTokenSource streamComplete = new CancellationTokenSource();
-			Task[] ThreadedTasks = new Task[concurrentTasks];
-			try {
-				for (int i = 0; i < concurrentTasks; i++) {
-					ThreadedTasks[i] = CreateCopyTask(streamComplete.Token, cancel);
-				}
-				foreach (FileInfo file in sourceDir.EnumerateFiles("*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = true })) {
-					string targetFilePath = Path.Combine(outputPath, Path.GetRelativePath(sourcePath, file.FullName));
-					if (!ReplaceAll && file.LastWriteTime <= new FileInfo(targetFilePath).LastWriteTime) continue;
-					copyJobs.Enqueue(new InOutPath(file.FullName, targetFilePath));
-				}
-				streamComplete.Cancel();
-				await Task.WhenAll(ThreadedTasks);
-			} finally {
-				foreach (Task task in ThreadedTasks) {
-					task.Dispose();
-				}
-				streamComplete.Dispose();
-			}
+		public bool Equals(InOutPath inOutPath) {
+			return source.Equals(inOutPath.source) && output.Equals(inOutPath.output);
+		}
+		public override bool Equals(object? obj) {
+			return obj is InOutPath && Equals(obj);
+		}
+
+		public override int GetHashCode() {
+			return System.HashCode.Combine(source, output);
+		}
+
+		public static bool operator ==(InOutPath left, InOutPath right) {
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(InOutPath left, InOutPath right) {
+			return !(left == right);
 		}
 	}
 }
--- A3SD-File-Worker/Program.cs
+++ A3SD-File-Worker/Program.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Caleb Sebastian Serafin">
+// Copyright (c) 2020 Caleb Sebastian Serafin. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the repository root for full license information.
+// </copyright>
+
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
 
 namespace A3SD_File_Worker {
 	class Program {
@@ -19,31 +23,35 @@ namespace A3SD_File_Worker {
 			}).Count();
 		}
 
-		public static async Task Main(string[] args) {
-			//string sourcePath = Console.ReadLine();
-			//string outputPath = Console.ReadLine();
-			//string sourcePath = @"F:\Documents";
+		public static async Task Main() {
+			// string sourcePath = Console.ReadLine();
+			// string outputPath = Console.ReadLine();
+			// string sourcePath = @"F:\Documents";
 			string sourcePath = @"F:\A3-doc-src";
 			string outputPath = @"F:\Doc2";
+
+			Console.WriteLine(CountAccessibleFiles(sourcePath) + " files in '" + sourcePath + "' ");
+			Console.WriteLine("Press key to start");
+			Console.ReadKey(); Console.WriteLine();
+			Console.WriteLine("Enter cancel to stop.");
+
 			Stopwatch s = new Stopwatch();
-			for (int i = 0; i < 4; i++) {
+
+			for (int i = 0; i < 1; i++) {
 				CancellationTokenSource cancel = new CancellationTokenSource();
-				Console.WriteLine(CountAccessibleFiles(sourcePath) + " files in '" + sourcePath + "' ");
 				FileCopyWorkerAsync fileCopier = new FileCopyWorkerAsync();
-				fileCopier.readWriteStreamBuffer = 4096 * (int)Math.Pow(2, i);
-				Console.WriteLine("readWriteStreamBuffer: " + (int)(4096 * Math.Pow(2, i)));
-				Console.WriteLine("Press key to start");
-				Console.ReadKey(); Console.WriteLine();
 				s.Restart();
-				await fileCopier.CreateCopyJobs(sourcePath, outputPath, cancel.Token);
+				fileCopier.EnqueueJob(sourcePath, outputPath);
+				await fileCopier.StartJobs(cancel.Token);
 				Console.WriteLine(s.ElapsedMilliseconds + "ms");
 				s.Stop();
+				new DirectoryInfo(outputPath).Delete(true);
+
 				cancel.Dispose();
-				Console.WriteLine(CountAccessibleFiles(outputPath) + " files in '" + outputPath + "' ");
 			}
+
 			Console.WriteLine("Exec Done. Press key to exit.");
 			Console.ReadKey(); Console.WriteLine();
-
 		}
 	}
 }
